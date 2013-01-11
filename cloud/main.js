60face4f4be13b3b314f30571baaf9a8d41ebf2a
Jeroen Leenarts <jeroen.leenarts@gmail.com>
2013-01-11 16:16:52 +0100

    Added save logic for NewsLetter object. -Add missing publishedAt date. -Send push on save.

--- cloud/main.js
+++ cloud/main.js
@@ -4,3 +4,30 @@
 Parse.Cloud.define("hello", function(request, response) {
   response.success("Hello world!");
 });
+
+Parse.Cloud.beforeSave("NewsLetter", function(request, response) {
+  var publishedAt = request.object.get("publishedAt");
+  if (publishedAt == null ) {
+    // set publishedAt if it's not set.
+    request.object.set("publishedAt", new Date());
+  }
+  response.success();  
+});
+
+Parse.Cloud.afterSave("NewsLetter", function(request) {
+	Parse.Push.send({
+	  channels: [ "" ],
+	  data: {
+	    alert: "Er is een nieuwe nieuwsbrief: " + request.object.get("name")
+	  }
+	}, {
+	  success: function() {
+		console.log("Push succeeded for newsletter " + request.object.get("name"));
+	  },
+	  error: function(error) {
+		console.error("Push failed for newsletter " + request.object.get("name") + " with error code: " + error.code + "and message: " + error.message);
+	  }
+	});
+});
+
+
