5ce6bfc89ed8e9338f28c6ba98cebf539891f430
Jeroen Leenarts <jeroen.leenarts@gmail.com>
2013-01-23 19:39:02 +0100

    Add user json script. -Also adds users to staff role.

---
+++ curl_scripts/addUser.sh
@@ -0,0 +1,41 @@
+#!/in/bash
+
+source ../../SebastiaanSchool/bin/setEnv.sh
+
+USER_NAME="$1"
+PASSWORD="$2"
+
+function jsonval {
+    temp=`echo $json | sed 's/\\\\\//\//g' | sed 's/[{}]//g' | awk -v k="text" '{n=split($0,a,","); for (i=1; i<=n; i++) print a[i]}' | sed 's/\"\:\"/\|/g' | sed 's/[\,]/ /g' | sed 's/\"//g' | grep -w $prop`
+    echo ${temp##*|}
+}
+
+json=`curl -s -X POST \
+ -H "X-Parse-Application-Id: ${PARSE_APPLICATION_ID}" \
+  -H "X-Parse-REST-API-Key: ${PARSE_REST_API_KEY}" \
+  -H "Content-Type: application/json" \
+  -d '{"username":"'${USER_NAME}'","password":"'${PASSWORD}'"}' \
+  https://api.parse.com/1/users`
+
+prop='objectId'
+userObjectId=`jsonval`
+
+curl -X PUT \
+  -H "X-Parse-Application-Id: ${PARSE_APPLICATION_ID}" \
+  -H "X-Parse-Master-Key: ${PARSE_MASTER_KEY}" \
+  -H "Content-Type: application/json" \
+  -d '{
+        "users": {
+          "__op": "AddRelation",
+          "objects": [
+            {
+              "__type": "Pointer",
+              "className": "_User",
+              "objectId": "'${userObjectId}'"
+            }
+          ]
+        }
+      }' \
+  https://api.parse.com/1/roles/BOlIXdHx2o
+
+echo ""
