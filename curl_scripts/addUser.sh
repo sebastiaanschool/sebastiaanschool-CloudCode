a31fc2625b820f1604742c538cefe97942e020c3
Jeroen Leenarts <jeroen.leenarts@gmail.com>
2013-02-24 22:42:09 +0100

    Don't use hard coded staff object id.

--- curl_scripts/addUser.sh
+++ curl_scripts/addUser.sh
@@ -9,15 +9,22 @@ function jsonval {
     temp=`echo $json | sed 's/\\\\\//\//g' | sed 's/[{}]//g' | awk -v k="text" '{n=split($0,a,","); for (i=1; i<=n; i++) print a[i]}' | sed 's/\"\:\"/\|/g' | sed 's/[\,]/ /g' | sed 's/\"//g' | grep -w $prop`
     echo ${temp##*|}
 }
+prop='objectId'
+
+json=`curl -X GET \
+  -H "X-Parse-Application-Id: ${PARSE_APPLICATION_ID}" \
+  -H "X-Parse-REST-API-Key: ${PARSE_REST_API_KEY}" \
+  https://api.parse.com/1/roles/`
+
+roleObjectId=`jsonval`
 
 json=`curl -s -X POST \
- -H "X-Parse-Application-Id: ${PARSE_APPLICATION_ID}" \
+  -H "X-Parse-Application-Id: ${PARSE_APPLICATION_ID}" \
   -H "X-Parse-REST-API-Key: ${PARSE_REST_API_KEY}" \
   -H "Content-Type: application/json" \
   -d '{"username":"'${USER_NAME}'","password":"'${PASSWORD}'"}' \
   https://api.parse.com/1/users`
 
-prop='objectId'
 userObjectId=`jsonval`
 
 curl -X PUT \
@@ -36,6 +43,6 @@ curl -X PUT \
           ]
         }
       }' \
-  https://api.parse.com/1/roles/BOlIXdHx2o
+  https://api.parse.com/1/roles/${roleObjectId}
 
 echo ""
