45471348490ed457c5e45fb0de40504ea1dd8e87
Jeroen Leenarts <jeroen.leenarts@gmail.com>
2013-03-26 20:41:29 +0100

    Added DEV/PROD parameter requirement to config generation.

--- config/genGlobalJson.sh
+++ config/genGlobalJson.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-source setCloudEnv.sh
+source setCloudEnv.sh $1
 
 sed -e "s/PARSE_APPLICATION_ID/${PARSE_APPLICATION_ID}/g" -e "s/PARSE_MASTER_KEY/${PARSE_MASTER_KEY}/g" global.json.template >global.json
