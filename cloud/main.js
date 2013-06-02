ef885ce4d998a0900a3206f1f35f9de957a138ab
Jeroen Leenarts <jeroen.leenarts@gmail.com>
2013-06-02 12:34:06 +0200

    Removed dummy hello cloud function.

--- cloud/main.js
+++ cloud/main.js
@@ -1,10 +1,3 @@
-
-// Use Parse.Cloud.define to define as many cloud functions as you want.
-// For example:
-Parse.Cloud.define("hello", function(request, response) {
-  response.success("Hello world!");
-});
-
 Parse.Cloud.beforeSave("NewsLetter", function(request, response) {
   var publishedAt = request.object.get("publishedAt");
   if (publishedAt == null ) {
