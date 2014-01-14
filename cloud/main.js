a6ff841e6ae53586d18b554ea4290ba28d73bb87
Jeroen Leenarts <jeroen.leenarts@gmail.com>
2014-01-14 09:29:22 +0100

    [cloud] Updated parse push to support android pushes.

--- cloud/main.js
+++ cloud/main.js
@@ -11,29 +11,58 @@ Parse.Cloud.afterSave("NewsLetter", function(request) {
     if (request.object.existed()) {
 		return;
 	}
+	
+	var iosQuery = new Parse.Query(Parse.Installation);
+	iosQuery.equalTo('deviceType', 'ios');
+	
 	Parse.Push.send({
 	  channels: [ "" ],
+	  where: iosQuery,
 	  data: {
 	    alert: "Er is een nieuwe nieuwsbrief: " + request.object.get("name"),
             badge: "Increment",
             t: 1
 	  }
 	}, {
-	  success: function() {
-		console.log("Push succeeded for newsletter " + request.object.get("name"));
-	  },
-	  error: function(error) {
-		console.error("Push failed for newsletter " + request.object.get("name") + " with error code: " + error.code + "and message: " + error.message);
-	  }
+  	  success: function() {
+  		console.log("iOS Push succeeded for newletter " + request.object.get("title"));
+  	  },
+  	  error: function(error) {
+  		console.error("iOS Push failed for newletter " + request.object.get("title") + " with error code: " + error.code + "and message: " + error.message);
+  	  }
 	});
+	
+	var androidQuery = new Parse.Query(Parse.Installation);
+	androidQuery.equalTo('deviceType', 'android');
+	
+	Parse.Push.send({
+	  channels: [ "newsletter" ],
+	  where: androidQuery,
+	  data: {
+	    alert: "Er is een nieuwe nieuwsbrief: " + request.object.get("name")
+	  }
+	}, {
+  	  success: function() {
+  		console.log("Android Push succeeded for newletter " + request.object.get("title"));
+  	  },
+  	  error: function(error) {
+  		console.error("Android Push failed for newletter " + request.object.get("title") + " with error code: " + error.code + "and message: " + error.message);
+  	  }
+	});
+	
 });
 
 Parse.Cloud.afterSave("Bulletin", function(request) {
     if (request.object.existed()) {
 		return;
 	}
+	
+	var iosQuery = new Parse.Query(Parse.Installation);
+	iosQuery.equalTo('deviceType', 'ios');
+	
 	Parse.Push.send({
-	  channels: [ "" ],
+	  channels: [ "bulletin" ],
+	  where: iosQuery,
 	  data: {
 	    alert: "Nieuwe mededeling: " + request.object.get("title"),
             badge: "Increment",
@@ -41,12 +70,31 @@ Parse.Cloud.afterSave("Bulletin", function(request) {
 	  }
 	}, {
 	  success: function() {
-		console.log("Push succeeded for bulletin " + request.object.get("title"));
+		console.log("iOS Push succeeded for bulletin " + request.object.get("title"));
 	  },
 	  error: function(error) {
-		console.error("Push failed for Bulletin " + request.object.get("title") + " with error code: " + error.code + "and message: " + error.message);
+		console.error("iOS Push failed for Bulletin " + request.object.get("title") + " with error code: " + error.code + "and message: " + error.message);
 	  }
 	});
+	
+	var androidQuery = new Parse.Query(Parse.Installation);
+	androidQuery.equalTo('deviceType', 'android');
+	
+	Parse.Push.send({
+	  channels: [ "" ],
+	  where: androidQuery,
+	  data: {
+	    alert: "Nieuwe mededeling: " + request.object.get("title")
+	  }
+	}, {
+	  success: function() {
+		console.log("Android Push succeeded for bulletin " + request.object.get("title"));
+	  },
+	  error: function(error) {
+		console.error("Android Push failed for Bulletin " + request.object.get("title") + " with error code: " + error.code + "and message: " + error.message);
+	  }
+	});
+	
 });
 
 
