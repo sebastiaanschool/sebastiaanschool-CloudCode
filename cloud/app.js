72690e13650755accf02cec87c91e01a30bb98c2
Jeroen Leenarts <jeroen.leenarts@gmail.com>
2014-02-15 10:32:57 +0100

    [web] Removed hello world gunk.

--- cloud/app.js
+++ cloud/app.js
@@ -25,17 +25,5 @@ app.get('/hello', function(req, res) {
   res.render('hello', { message: 'Congrats, you just set up your app!' });
 });
 
-// // Example reading from the request query string of an HTTP get request.
-// app.get('/test', function(req, res) {
-//   // GET http://example.parseapp.com/test?message=hello
-//   res.send(req.query.message);
-// });
-
-// // Example reading from the request body of an HTTP post request.
-// app.post('/test', function(req, res) {
-//   // POST http://example.parseapp.com/test (with request body "message=hello")
-//   res.send(req.body.message);
-// });
-
 // Attach the Express app to Cloud Code.
 app.listen();
