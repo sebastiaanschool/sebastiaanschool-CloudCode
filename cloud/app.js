fc8807a4ee17d0352a5f1c436994407962ff8152
Jeroen Leenarts <jeroen.leenarts@gmail.com>
2014-02-15 10:32:40 +0100

    [web] Added Parse cookie middleware.

--- cloud/app.js
+++ cloud/app.js
@@ -2,6 +2,7 @@
 // These two lines are required to initialize Express in Cloud Code.
 var express = require('express');
 var parseExpressHttpsRedirect = require('parse-express-https-redirect');
+var parseExpressCookieSession = require('parse-express-cookie-session');
 
 var newslettersController = require('cloud/controllers/newsletters.js');
 
@@ -11,6 +12,8 @@ var app = express();
 app.set('views', 'cloud/views');  // Specify the folder to find templates
 app.set('view engine', 'ejs');    // Set the template engine
 app.use(express.bodyParser());    // Middleware for reading request body
+app.use(express.cookieParser('Ns66$yQmAs{6R>9O6CFFOg40PoZeJh'));
+app.use(parseExpressCookieSession({ cookie: { maxAge: 3600000 } }));
 app.use(parseExpressHttpsRedirect()); // Middleware to redirect all requests to use https.
 
 // RESTful route for the newsletter object.
